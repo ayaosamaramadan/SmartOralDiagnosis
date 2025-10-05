27b25ac75e055dc8d98fa5ff84b9b0b37cd6c0ba
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-10-05 23:27:05 +0300

    add data of two disease

---
+++ frontend/data/AllOrals.ts
@@ -0,0 +1,258 @@
+const AllOrals = [
+    {
+        id: 1,
+        title: "Canker Sores",
+        shortTitle: "CaS",
+        description: "Description for Canker Sores",
+        img: [
+            "assets/CaS/CaS.jpg",
+        ],
+        overview: "Canker sores, also called aphthous ulcers, are small, shallow lesions that develop on the soft tissues in your mouth or at the base of your gums. Unlike cold sores, canker sores don't occur on the surface of your lips and they aren't contagious. They can be painful, however, and can make eating and talking difficult. Most canker sores go away on their own in a week or two. Check with your doctor or dentist if you have unusually large or painful canker sores or canker sores that don't seem to heal.",
+        symptoms: {
+            title: "Most canker sores are round or oval with a white or yellow center and a red border. They form inside your mouth — on or under your tongue, inside your cheeks or lips, at the base of your gums, or on your soft palate. You might notice a tingling or burning sensation a day or two before the sores actually appear. There are several types of canker sores, including minor, major and herpetiform sores.",
+            list: [
+                {
+                    id: 18, type: "Minor canker sores",
+                    desc: "Minor canker sores are the most common and:",
+                    dots: [
+                        "Are usually small",
+                        "Are oval shaped with a red edge",
+                        "Heal without scarring in one to two weeks"
+                    ]
+                },
+                {
+                    id: 18, type: "Major canker sores",
+                    desc: "Major canker sores are less common and:",
+                    dots: [
+                        "Are larger and deeper than minor canker sores",
+                        "Are usually round with defined borders, but may have irregular edges when very large",
+                        "Can be extremely painful",
+                        "May take up to six weeks to heal and can leave extensive scarring"
+                    ]
+                },
+                {
+                    id: 18, type: "Herpetiform canker sores",
+                    desc: "Herpetiform canker sores are uncommon and usually develop later in life, but they're not caused by herpes virus infection. These canker sores:",
+                    dots: [
+                        "Are pinpoint size",
+                        "Often occur in clusters of 10 to 100 sores, but may merge into one large ulcer",
+                        "Have irregular edges",
+                        "Heal without scarring in one to two weeks"
+                    ]
+                },
+            ],
+
+            WhenSeeDoctor: [
+                {
+                    title: "Consult your doctor if you experience:",
+                    list: [
+                        "Unusually large canker sores",
+                        "Recurring sores, with new ones developing before old ones heal, or frequent outbreaks",
+                        "Persistent sores, lasting two weeks or more",
+                        "Sores that extend into the lips themselves (vermilion border)",
+                        "Pain that you can't control with self-care measures",
+                        "Extreme difficulty eating or drinking",
+                        "High fever along with canker sores"
+                    ],
+                    note: "See your dentist if you have sharp tooth surfaces or dental appliances that seem to trigger the sores."
+                },
+            ]
+        },
+        causes: {
+            title: "The precise cause of canker sores remains unclear, though researchers suspect that a combination of factors contributes to outbreaks, even in the same person.",
+            triggers: [
+                {
+                    title: "Possible triggers for canker sores include:",
+                    list: [
+                        "A minor injury to your mouth from dental work, overzealous brushing, sports mishaps or an accidental cheek bite",
+                        "Toothpastes and mouth rinses containing sodium lauryl sulfate",
+                        "Food sensitivities, particularly to chocolate, coffee, strawberries, eggs, nuts, cheese, and spicy or acidic foods",
+                        "A diet lacking in vitamin B-12, zinc, folate (folic acid) or iron",
+                        "An allergic response to certain bacteria in your mouth",
+                        "Helicobacter pylori, the same bacteria that cause peptic ulcers",
+                        "Hormonal shifts during menstruation",
+                        "Emotional stress"
+                    ]
+                }
+            ],
+            conditions: {
+                title: "Canker sores may also occur because of certain conditions and diseases, such as:",
+                list: [
+                    "Celiac disease, a serious intestinal disorder caused by a sensitivity to gluten, a protein found in most grains",
+                    "Inflammatory bowel diseases, such as Crohn's disease and ulcerative colitis",
+                    "Behcet's disease, a rare disorder that causes inflammation throughout the body, including the mouth",
+                    "A faulty immune system that attacks healthy cells in your mouth instead of pathogens, such as viruses and bacteria",
+                    "HIV/AIDS, which suppresses the immune system"
+                ]
+            },
+            note: "Unlike cold sores, canker sores are not associated with herpes virus infections."
+        },
+        riskFactors: "Anyone can develop canker sores. But they occur more often in teens and young adults, and they're more common in females. Often people with recurrent canker sores have a family history of the disorder. This may be due to heredity or to a shared factor in the environment, such as certain foods or allergens.",
+        prevwntion: {
+            title: "Canker sores often recur, but you may be able to reduce their frequency by following these tips:",
+            list: [
+                {
+                    id: 1,
+                    tip: "Watch what you eat.",
+                    desc: "Try to avoid foods that seem to irritate your mouth. These may include nuts, chips, pretzels, certain spices, salty foods and acidic fruits, such as pineapple, grapefruit and oranges. Avoid any foods to which you're sensitive or allergic."
+                },
+                {
+                    id: 2,
+                    tip: "Choose healthy foods.",
+                    desc: "To help prevent nutritional deficiencies, eat plenty of fruits, vegetables and whole grains."
+                },
+                {
+                    id: 3,
+                    tip: "Follow good oral hygiene habits.",
+                    desc: "Regular brushing after meals and flossing once a day can keep your mouth clean and free of foods that might trigger a sore. Use a soft brush to help prevent irritation to delicate mouth tissues, and avoid toothpastes and mouth rinses that contain sodium lauryl sulfate."
+                },
+                {
+                    id: 4,
+                    tip: "Protect your mouth.",
+                    desc: "If you have braces or other dental appliances, ask your dentist about orthodontic waxes to cover sharp edges."
+                },
+                {
+                    id: 5,
+                    tip: "Reduce your stress.",
+                    desc: "If your canker sores seem to be related to stress, learn and use stress-reduction techniques, such as meditation and guided imagery."
+                }
+            ]
+        }
+    },
+
+    {
+        id: 2,
+        title: "Cold Sores",
+        shortTitle: "CoS",
+        description: "Description for Cold Sores",
+        img: [
+            "assets/CoS/CoS.avif",
+        ],
+        overview: "Cold sores, or fever blisters, are a common viral infection. They are tiny, fluid-filled blisters on and around the lips. These blisters are often grouped together in patches. After the blisters break, a scab forms that can last several days. Cold sores usually heal in 2 to 3 weeks without leaving a scar. Cold sores spread from person to person by close contact, such as kissing. They're usually caused by herpes simplex virus type 1 (HSV-1), and less commonly herpes simplex virus type 2 (HSV-2). Both of these viruses can affect the mouth or genitals and can be spread by oral sex. The virus can spread even if you don't see the sores. There's no cure for cold sores, but treatment can help manage outbreaks. Prescription antiviral medicine or creams can help sores heal more quickly. And they may make future outbreaks happen less often and be shorter and less serious.",
+        symptoms: {
+            title: "A cold sore usually passes through several stages:",
+            list: [
+                {
+                    id: 18, type: "Tingling and itching.",
+                    desc: "Many people feel itching, burning or tingling around the lips for a day or so before a small, hard, painful spot appears and blisters form.",
+
+                },
+                {
+                    id: 18, type: "Blisters.",
+                    desc: "Small fluid-filled blisters often form along the border of the lips. Sometimes they appear around the nose or cheeks or inside the mouth.",
+                },
+                {
+                    id: 18, type: "Oozing and crusting.",
+                    desc: " The small blisters may merge and then burst. This can leave shallow open sores that ooze and crust over."
+                },
+            ],
+
+            additionalTitle: [
+                {
+                    title: "Symptoms vary, depending on whether this is your first outbreak or a recurrence. The first time you have a cold sore, symptoms may not start for up to 20 days after you were first exposed to the virus. The sores can last several days. And the blisters can take 2 to 3 weeks to heal completely. If blisters return, they'll often appear at the same spot each time and tend to be less severe than the first outbreak. In a first-time outbreak, you also might experience:",
+                    list: [
+                        "Fever",
+                        "Painful gums.",
+                        "Sore throat.",
+                        "Headache.",
+                        "Muscle aches.",
+                        "Swollen lymph nodes."
+                    ],
+                    note: "Children under 5 years old may have cold sores inside their mouths. These sores are often mistaken for canker sores. Canker sores involve only the mucous membrane and aren't caused by the herpes simplex virus."
+
+                }
+            ],
+            WhenSeeDoctor: [
+                {
+                    title: "Cold sores generally clear up without treatment. See your health care provider if:",
+                    list: [
+                        "You have a weak immune system.",
+                        "The cold sores don't heal within two weeks.",
+                        "Symptoms are severe.",
+                        "The cold sores often return.",
+                        "You have gritty or painful eyes."
+                    ],
+                },
+            ]
+        },
+        causes: {
+            title: "Cold sores are caused by certain strains of the herpes simplex virus (HSV). HSV-1 usually causes cold sores. HSV-2 is often the cause of genital herpes. But either type can spread to the face or genitals through close contact, such as kissing or oral sex. Shared eating utensils, razors and towels can also spread HSV-1. Cold sores are most likely to spread when you have oozing blisters. But you can spread the virus even if you don't have blisters. Many people who are infected with the virus that causes cold sores never develop symptoms.",
+            triggers: [
+                {
+                    title: "Once you've had a herpes infection, the virus can hide in nerve cells in the skin and may cause another cold sore at the same place as before. A return of cold sores may be triggered by:",
+                    list: [
+                        "Viral infection or fever.",
+                        "Hormonal changes, such as those related to a menstrual period.",
+                        "Stress.",
+                        "Fatigue.",
+                        "Being in the sun or wind.",
+                        "Changes in the immune system.",
+                        "Injury to the skin."
+                    ]
+                }
+            ],
+        },
+        riskFactors: {
+            title: "Almost everyone is at risk of cold sores. Most adults carry the virus that causes cold sores, even if they've never had symptoms.",
+            triggers: [
+                {
+                    title: "You're most at risk of complications from the virus if you have a weak immune system from conditions and treatments such as:",
+                    list: [
+                        "HIV/AIDS.",
+                        "Atopic dermatitis (eczema).",
+                        "Cancer chemotherapy.",
+                        "Anti-rejection medicine for organ transplants."
+                    ]
+                }
+            ],
+        },
+        complications: {
+            title: "In some people, the virus that causes cold sores can cause problems in other areas of the body, including:",
+            list: [
+                {
+                    id: 1,
+                    title: "Fingertips",
+                    desc: "Herpes whitlow is an infection of the fingers. Children who suck their thumbs may transfer the infection from their mouths to their thumbs."
+                },
+                {
+                    id: 2,
+                    title: "Eyes",
+                    desc: "The virus can cause eye infection (herpes keratitis), which may lead to pain, tearing, light sensitivity, and even vision loss if not treated."
+                },
+                {
+                    id: 3,
+                    title: "Widespread skin infection",
+                    desc: "People with skin conditions such as eczema are at risk of the virus spreading over large areas of their body."
+                },
+                {
+                    id: 4,
+                    title: "Other organs",
+                    desc: "In rare cases, the virus can affect the spinal cord and brain (herpes encephalitis), especially in people with weakened immune systems."
+                }
+            ],
+            disc: "Complications from cold sores are uncommon but can be serious, especially in people with weakened immune systems or underlying skin conditions. Prompt medical attention is important if symptoms spread beyond the mouth or lips."
+
+        },
+        prevention: {
+            title: "Your health care provider may prescribe an antiviral medicine for you to take on a regular basis if you develop cold sores more than nine times a year or if you're at high risk of serious complications. If sunlight seems to trigger your condition, apply sunblock to the spot where the cold sore tends to form. Or talk with your health care provider about using an oral antiviral medicine before you do an activity that tends to cause a cold sore to return. Take these steps to help avoid spreading cold sores to other people:",
+            list: [
+              
+                {
+                    id: 1,
+                    tip: "Avoid kissing and skin contact with people while blisters are present.",
+                    desc: "The virus spreads most easily when the blisters leak fluid."
+                },
+                {
+                    id: 2,
+                    tip: "Avoid sharing items.",
+                    desc: "Utensils, towels, lip balm and other personal items can spread the virus when blisters are present."
+                },
+                {
+                    id: 3,
+                    tip: "Keep your hands clean.",
+                    desc: "When you have a cold sore, wash your hands carefully before touching yourself and other people, especially babies."
+                }
+            ]
+        }
+    },
+];
