cf0470065ea5ae3e503c18f396bc0f6a77eaa915
ayaosamaramadan <ayaramadan2011@gmail.com>
2025-11-10 21:10:12 +0200

    Add vercel config

--- BackEnd/obj/Debug/net8.0/MedicalManagement.API.AssemblyInfo.cs
+++ BackEnd/obj/Debug/net8.0/MedicalManagement.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedicalManagement.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5ee5b54256da70c28bbe11fdb81a78e135f04d18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9baa81932c24a3bd9d0fb22bba6b0b1f583ff6a4")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedicalManagement.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedicalManagement.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
